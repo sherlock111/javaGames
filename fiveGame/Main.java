5172e00b0240aaba4fdbf62567082a34e1219997
William.Forever <zhaozhonyu@huawei.com>
2017-04-10 21:18:54 +0800

    Create Main.java

---
+++ fiveGame/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("five2.fxml"));
+        primaryStage.setTitle("五子棋");
+        Image imageBG = new Image("sample/bg_game.JPG");
+        /*Group root = new Group();
+        //Image imageBG = new Image("sample/bg_game.JPG");
+        Canvas canvas = new Canvas(imageBG.getWidth(), imageBG.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.drawImage(imageBG, 0, 0, imageBG.getWidth(), imageBG.getHeight());
+        gc.drawImage(new Image("sample/W.png"), 26, 42);
+        canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println(event.getX());
+                System.out.println(event.getY());
+                System.out.println(event.getSceneX());
+                System.out.println(event.getSceneY());
+            }
+        });
+        root.getChildren().add(canvas);*/
+
+        primaryStage.setScene(new Scene(root, imageBG.getWidth(), imageBG.getHeight()));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
